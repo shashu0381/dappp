ead21e4ae6b9a2bf3f1d27c6bc8ab8c1d21bf12a
shashu0381 <161243428+shashu0381@users.noreply.github.com>
2024-08-18 21:17:50 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -4,19 +4,33 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
-    string public networkAddress;
+
+    struct Question {
+        string question;
+        uint256 answer; // The answer will be a number
+        uint256 points; // Points awarded for the correct answer
+    }
+
+    Question[] public quiz;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-    event NetworkAddressChanged(string oldAddress, string newAddress);
+    event QuestionRemoved(uint256 index);
+    event ScoreCalculated(uint256 score);
 
-    constructor(uint initBalance, string memory initialAddress) payable {
+    constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
-        networkAddress = initialAddress;
+
+        // Initialize with some questions
+        quiz.push(Question("How many years is a typical mortgage?", 30, 1));
+        quiz.push(Question("What is the minimum credit score to get a good mortgage rate?", 700, 1));
+        quiz.push(Question("What is the average annual return of the S&P 500?", 10, 1));
+        quiz.push(Question("How many years does it take for a bond to mature?", 10, 1));
+        quiz.push(Question("What is the typical down payment percentage for a house?", 20, 1));
     }
 
-    function getBalance() public view returns (uint256) {
+    function getBalance() public view returns(uint256){
         return balance;
     }
 
@@ -59,21 +73,29 @@ contract Assessment {
         emit Withdraw(_withdrawAmount);
     }
 
-    function changeNetworkAddress(string memory newAddress) public {
+    function calculateScore(uint256[] memory answers) public view returns (uint256) {
+        require(answers.length == quiz.length, "Number of answers must match number of questions");
+        uint256 score = 0;
+
+        for (uint256 i = 0; i < quiz.length; i++) {
+            if (answers[i] == quiz[i].answer) {
+                score += quiz[i].points;
+            }
+        }
+
+        return score;
+    }
+
+    function removeQuestion(uint256 index) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        string memory oldAddress = networkAddress;
-        networkAddress = newAddress;
-        emit NetworkAddressChanged(oldAddress, newAddress);
-    }
+        require(index < quiz.length, "Invalid question index");
 
-    function getNetworkAddress() public view returns (string memory) {
-        return networkAddress;
-    }
+        // Remove the question by shifting all subsequent questions left
+        for (uint256 i = index; i < quiz.length - 1; i++) {
+            quiz[i] = quiz[i + 1];
+        }
+        quiz.pop();
 
-    function generateQRCode() public view returns (string memory) {
-        // This function generates a URL for a QR code using Google's Chart API.
-        // However, this URL must be processed on the client side to display the QR code.
-        return string(abi.encodePacked("https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=", networkAddress));
+        emit QuestionRemoved(index);
     }
 }
-
